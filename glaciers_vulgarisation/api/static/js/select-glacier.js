c5099ee750f9193346ef0d12d09ed3e4a0f55310
Lea-drd <73934530+Lea-drd@users.noreply.github.com>
2024-05-07 07:45:00 +0200

    elevation mer ssp scenario

--- glaciers_vulgarisation/api/static/js/select-glacier.js
+++ glaciers_vulgarisation/api/static/js/select-glacier.js
@@ -74,3 +74,19 @@ function openTabELA(evt, glacierName) {
     evt.currentTarget.className += " active";
     evt.preventDefault(); // Pour empêcher le comportement par défaut du lien
   }
+
+  function openSSP(evt, sspName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent-ssp");
+    console.log(tabcontent)
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(sspName).style.display = "block";
+    evt.currentTarget.className += " active";
+    evt.preventDefault(); // Pour empêcher le comportement par défaut du lien
+  }
